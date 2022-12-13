d6b02a9d105b236a2496898a014a006f6df2bd11
Awfa Afandi <amaa.vip@gmail.com>
2022-12-13 09:57:57 +0300

    share image added

--- app/src/main/java/com/amaa/grocery/Activitydetails.kt
+++ app/src/main/java/com/amaa/grocery/Activitydetails.kt
@@ -2,8 +2,11 @@ package com.amaa.grocery
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log.d
 import android.widget.ImageButton
 import android.widget.ImageView
@@ -20,15 +23,16 @@ class Activitydetails : AppCompatActivity() {
         setContentView(R.layout.activity_activitydetails)
 
         val textv = findViewById<TextView>(R.id.textdet)
-        val id = intent?.extras?.getString("Letter").toString()
-        textv.text = id
+        val ImageTitle = intent?.extras?.getString("Letter").toString()
+        textv.text = ImageTitle
 
         val texds = findViewById<TextView>(R.id.textView2)
-        val id2 = intent?.extras?.getString("Descr").toString()
-        texds.text = id2
+        val ImageDescrption = intent?.extras?.getString("Descr").toString()
+        texds.text = ImageDescrption
 
         val imageView = findViewById<ImageView>(R.id.imageView)
-       imageView.setImageResource(intent.extras!!.getInt("image"))
+      val img = intent.extras!!.getInt("image")
+       imageView.setImageResource(img)
 
 
         val shareButton = findViewById<ImageButton>(R.id.imageButton)
@@ -38,10 +42,14 @@ class Activitydetails : AppCompatActivity() {
 
 
             val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "image/*"
+                val bitmap = BitmapFactory.decodeResource(resources,img )
+              //  val path = MediaStore.Images.Media.insertImage(contentResolver , bitmap , "$id"  , "$id2" )
+                val path = MediaStore.Images.Media.insertImage(contentResolver , bitmap , "$ImageTitle"  , "$ImageDescrption" )
+                val uri = Uri.parse(path)
+                putExtra(Intent.EXTRA_STREAM , uri)
+                putExtra(Intent.EXTRA_TEXT , "$ImageTitle")
 
-                action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT , "$id")
-                type = "text/*"
 
 
 
